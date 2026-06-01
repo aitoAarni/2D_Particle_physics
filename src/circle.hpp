9a8458cb4988c55c106b844b5e709bd669d709cd
aitoAarni <aarni.rechardt@gmail.com>
2026-06-01 22:25:52 +0300

    rendering a circle works

--- src/circle.hpp
+++ src/circle.hpp
@@ -1,5 +1,9 @@
 #include <vector>
 #include <SDL2/SDL.h>
+#include <cmath>
+#include <iostream>
+#include <format>
+// #include <algorithm>
 
 class Circle {
     public:
@@ -10,8 +14,16 @@ class Circle {
 };
 
 void fill_circle(SDL_Renderer* renderer, Circle& circle) {
-    
+    int r_pow_2 = circle.radius * circle.radius;
+    int x_pow_2 = circle.x*circle.x;
+    int y_pow_2 = circle.y*circle.y;
+    int b = 2 * circle.x;
+    int b_pow_2 = b * b;
     for (int y = circle.y-circle.radius; y <= circle.y + circle.radius; y++) {
-        
+        int c = x_pow_2 + (circle.y - y) * (circle.y - y) - r_pow_2;
+        int x_0 = b + std::sqrt(b_pow_2 - 4*c);
+        int x_1 = b - std::sqrt(b_pow_2 - 4*c);
+        std::cout << std::format("line from ({}, {}) to ({}, {})\n", std::min(x_0, x_1), y, std::max(x_0, x_1), y);
+        SDL_RenderDrawLine(renderer, std::min(x_0, x_1)/2, y, std::max(x_0, x_1)/2, y);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,48 +3,50 @@
 #include "tools.hpp"
 #include "circle.hpp"
 
-
-
-int main(int argc, char* args[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+int main(int argc, char *args[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
         std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << "\n";
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
+    SDL_Window *window = SDL_CreateWindow(
         "C++ Physics Sandbox",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         1200, 1000,
-        SDL_WINDOW_SHOWN
-    );
+        SDL_WINDOW_SHOWN);
 
-    if (window == nullptr) {
+    if (window == nullptr)
+    {
         std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << "\n";
         SDL_Quit();
         return -1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(
-        window, -1, 
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+        window, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-    if (renderer == nullptr) {
+    if (renderer == nullptr)
+    {
         std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << "\n";
         SDL_DestroyWindow(window);
         SDL_Quit();
         return -1;
     }
 
-
     bool isRunning = true;
     SDL_Event event;
     Circle circle(600, 500, 50);
 
-    while (isRunning) {
+    while (isRunning)
+    {
 
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
                 isRunning = false;
             }
         }
