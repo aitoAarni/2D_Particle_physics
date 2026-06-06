598fc5b8dd6de1006703eabaebebbf4d0b22f169
aitoAarni <aarni.rechardt@gmail.com>
2026-06-06 15:49:29 +0300

    add contact normal func

--- src/collision_resolver.hpp
+++ src/collision_resolver.hpp
@@ -1,3 +1,12 @@
 #pragma once
 #include "circle.hpp"
-#include "cmath"
+#include "structs.hpp"
+#include <cmath>
+
+Vector contact_normal(Circle &a, Circle &b)
+{
+    float y{b.get_y() - a.get_y()};
+    float x{b.get_x() - a.get_x()};
+    float dist{std::hypot(x, y)};
+    return {x / dist, y / dist};
+}
