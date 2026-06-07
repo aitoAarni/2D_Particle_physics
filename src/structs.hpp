3e77a98ffc712b102dda0585a9c2b109415a4de0
aitoAarni <aarni.rechardt@gmail.com>
2026-06-07 10:00:57 +0300

    refactor

--- src/structs.hpp
+++ src/structs.hpp
@@ -5,34 +5,34 @@ struct Vector
 {
     float x;
     float y;
+
+    Vector operator-(Vector &other)
+    {
+        return {x - other.x, y - other.y};
+    }
+    
+    Vector operator+(Vector &other)
+    {
+        return {x + other.x, y + other.y};
+    }
+    
+    float operator*(Vector &other)
+    {
+        return x * other.x + y * other.y;
+    }
+    
+    Vector operator*(float scalar)
+    {
+        return {x * scalar, y * scalar};
+    }
+    
+    Vector operator-()
+    {
+        return {-x, -y};
+    }
 };
 
 std::ostream &operator<<(std::ostream &os, Vector vec)
 {
     return os << "x: " << vec.x << ", y: " << vec.y;
-}
-
-Vector operator-(Vector &a, Vector &b)
-{
-    return {a.x - b.x, a.y - b.y};
-}
-
-Vector operator+(Vector &a, Vector &b)
-{
-    return {a.x + b.x, a.y + b.y};
-}
-
-float operator*(Vector &a, Vector &b)
-{
-    return a.x * b.x + a.y * b.y;
-}
-
-Vector operator*(Vector &vec, float scalar)
-{
-    return {vec.x * scalar, vec.y * scalar};
-}
-
-Vector operator-(const Vector &vec)
-{
-    return {-vec.x, -vec.y};
 }
